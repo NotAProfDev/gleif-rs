e0234ff3ab0c43a6d13091a4ab460681f002cb69
NotAProfDev <84450364+NotAProfDev@users.noreply.github.com>
2025-01-12 19:08:04 +0000

    update dependencies and enhance RequestBuilder with improved error handling; add tests for request functionality

--- src/api/request_builder.rs
+++ src/api/request_builder.rs
@@ -45,7 +45,7 @@ impl<'a> RequestBuilder<'a> {
             })?;
 
         info!(
-            "Sending {} request to URL: {} with filter {:?}",
+            "Sending {} request to URL: {} with query parameters: {:?}",
             self.method, url, self.query_params
         );
         let mut request = self.api_client.client.request(self.method.clone(), url);
@@ -67,29 +67,197 @@ impl<'a> RequestBuilder<'a> {
         );
 
         if response.status().is_success() {
-            if response
+            let content_type = response
                 .headers()
                 .get(reqwest::header::CONTENT_TYPE)
-                .map_or(false, |v| v == "application/json")
-            {
+                .map_or_else(
+                    || "No content type".to_string(),
+                    |v| v.to_str().unwrap_or("Invalid content type").to_string(),
+                );
+
+            if content_type == "application/json" {
                 let json = response.json::<T>().await.map_err(|e| {
                     error!("Deserialization error: {}", e);
                     ApiError::DeserializationError(e)
                 })?;
                 Ok(json)
             } else {
-                Err(ApiError::ResponseTypeError(
-                    response
-                        .headers()
-                        .get(reqwest::header::CONTENT_TYPE)
-                        .map_or("No content type", |v| v.to_str().unwrap())
-                        .to_owned(),
-                ))
+                Err(ApiError::ResponseTypeError(content_type))
             }
         } else {
             Err(ApiError::RequestError(
                 response.error_for_status().unwrap_err(),
             ))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::api::client::ApiClient;
+    use mockito;
+    use reqwest::Method;
+    use serde::Deserialize;
+    use std::collections::HashMap;
+
+    #[derive(Deserialize, Debug, PartialEq)]
+    struct TestResponse {
+        message: String,
+    }
+
+    struct TestSetup {
+        api_client: ApiClient,
+        endpoint: &'static str,
+        method: Method,
+        server: mockito::ServerGuard,
+    }
+
+    /// Sets up the test environment with a mock server and an ApiClient.
+    fn setup() -> TestSetup {
+        let server = mockito::Server::new();
+        let url = server.url();
+
+        let api_client = ApiClient::new(&url).expect("Failed to create ApiClient");
+        let endpoint = "/test";
+        let method = Method::GET;
+
+        TestSetup {
+            api_client,
+            endpoint,
+            method,
+            server,
+        }
+    }
+
+    /// Sets up the test environment with a asynchronously mock server and an ApiClient.
+    async fn setup_async() -> TestSetup {
+        let server = mockito::Server::new_async().await;
+        let url = server.url();
+
+        let api_client = ApiClient::new(&url).expect("Failed to create ApiClient");
+        let endpoint = "/test";
+        let method = Method::GET;
+
+        TestSetup {
+            api_client,
+            endpoint,
+            method,
+            server,
+        }
+    }
+
+    /// Tests the creation of a new RequestBuilder.
+    #[test]
+    fn test_request_builder_new() {
+        let setup = setup();
+
+        let request_builder =
+            RequestBuilder::new(&setup.api_client, setup.endpoint, setup.method.clone());
+
+        assert_eq!(
+            request_builder.api_client.base_url(),
+            setup.api_client.base_url()
+        );
+        assert_eq!(request_builder.endpoint, setup.endpoint);
+        assert_eq!(request_builder.method, setup.method);
+        assert_eq!(request_builder.query_params, HashMap::new());
+    }
+
+    /// Tests the addition of a query parameter to the RequestBuilder.
+    #[test]
+    fn test_request_builder_filter() {
+        let setup = setup();
+
+        let mut request_builder =
+            RequestBuilder::new(&setup.api_client, setup.endpoint, setup.method.clone());
+        request_builder = request_builder.filter("key", "value");
+
+        let mut expected_query_params = HashMap::new();
+        expected_query_params.insert("key".to_string(), "value".to_string());
+
+        assert_eq!(request_builder.query_params, expected_query_params);
+    }
+
+    /// Tests sending a request successfully and receiving a valid response.
+    #[tokio::test]
+    async fn test_request_builder_send_success() {
+        let mut setup = setup_async().await;
+
+        let _m = setup
+            .server
+            .mock("GET", "/test")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{"message": "success"}"#)
+            .create();
+
+        let request_builder =
+            RequestBuilder::new(&setup.api_client, setup.endpoint, setup.method.clone());
+        let response: TestResponse = request_builder
+            .send()
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(
+            response,
+            TestResponse {
+                message: "success".to_string()
+            }
+        );
+    }
+
+    /// Tests sending a request that results in a server error.
+    #[tokio::test]
+    async fn test_request_builder_send_request_error() {
+        let mut setup = setup_async().await;
+
+        let _m = setup.server.mock("GET", "/test").with_status(500).create();
+
+        let request_builder =
+            RequestBuilder::new(&setup.api_client, setup.endpoint, setup.method.clone());
+        let result: Result<TestResponse, ApiError> = request_builder.send().await;
+
+        assert!(matches!(result, Err(ApiError::RequestError(_))));
+    }
+
+    /// Tests sending a request that results in a deserialization error.
+    #[tokio::test]
+    async fn test_request_builder_send_deserialization_error() {
+        let mut setup = setup_async().await;
+
+        let _m = setup
+            .server
+            .mock("GET", "/test")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(r#"{"invalid": "response"}"#)
+            .create();
+
+        let request_builder =
+            RequestBuilder::new(&setup.api_client, setup.endpoint, setup.method.clone());
+        let result: Result<TestResponse, ApiError> = request_builder.send().await;
+
+        assert!(matches!(result, Err(ApiError::DeserializationError(_))));
+    }
+
+    /// Tests sending a request that results in a response type error.
+    #[tokio::test]
+    async fn test_request_builder_send_response_type_error() {
+        let mut setup = setup_async().await;
+
+        let _m = setup
+            .server
+            .mock("GET", "/test")
+            .with_status(200)
+            .with_header("content-type", "text/plain")
+            .with_body("Not JSON")
+            .create();
+
+        let request_builder =
+            RequestBuilder::new(&setup.api_client, setup.endpoint, setup.method.clone());
+        let result: Result<TestResponse, ApiError> = request_builder.send().await;
+
+        assert!(matches!(result, Err(ApiError::ResponseTypeError(_))));
     }
 }
